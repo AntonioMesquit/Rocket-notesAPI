6d05edfc40c41ec319f4fd51c78cb59a16d0ebd6
antonio <antonio109mesquita@gmail.com>
2023-09-17 20:44:54 -0300

    up

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -32,6 +32,57 @@ class NotesController {
 
     response.json()
   }
-}
+  async show(request, response) {
+    const { id } = request.params
 
+    const note = await knex("notes").where({ id}).first()
+    const tags = await knex("tags").where({note_id: id}).orderBy("name")
+    const links = await knex("links").where({note_id: id}).orderBy("created_at")
+
+    return response.json({
+      ...note,
+      tags,
+      links,
+    })
+
+  }
+  async delete(request, response) {
+    const { id } = request.params
+    await knex("notes").where({id}).delete()
+
+    return response.json("Nota deletada com sucesso")
+}
+async index(request, response) {
+  const { title, user_id , tags} = request.query
+
+  let notes;
+
+  if(tags){
+   const filterTags = tags.split(',').map(tag => tag.trim());
+    notes = await knex("tags")
+    .select([
+      "notes.id",
+      "notes.title",
+      "notes.user_id",
+    ])
+    .where("notes.user_id", user_id)
+    .whereLike("notes.title", `%${title}%`)
+    .whereIn("name" , filterTags)
+    .innerJoin("notes" , "notes.id" , "tags.note_id")
+    .orderBy("notes.title")
+
+
+  }else{
+
+  notes = await knex("notes")
+  .where({ user_id})
+  .whereLike("title" , `%${title}%`) // funcao de pesquisar
+  .orderBy("title")
+  }
+
+    
+ 
+  return response.json(notes)
+}
+}
 module.exports = NotesController
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,6 +7,9 @@ const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/', notesController.index)
+notesRoutes.get('/:id', notesController.show)
+notesRoutes.delete('/:id', notesController.delete)
 
 
 module.exports = notesRoutes
